ec8ed2e96394175a80abfe2cb9d8324519107209
parin <hopper.ken42@gmail.com>
2020-11-16 16:01:17 +0700

    new height

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const userInfoRef = database.ref("userInfo");
 new Vue({
     el:"#userInfo",
     data:{
-        weight:'',
         height:'',
         shouderW:'',
         infos:[],
@@ -15,7 +14,6 @@ new Vue({
         
         editInfo:function(info){
             this.editText=info
-            this.weight=info.weight
             this.height=info.height
             this.shouderW=info.shouderW
             
@@ -23,18 +21,17 @@ new Vue({
 
         updateInfo:function(info){
             this.editText=info
-            this.weight=info.weight
             this.height=info.height
             this.shouderW=info.shouderW
             
-            userInfoRef.child('MLs2pz2HisdOvzC6VRR').update({weight:this.weight,height:this.height,shouderW:this.shouderW})
+            userInfoRef.child('MLs2pz2HisdOvzC6VRR').update({height:this.height,shouderW:this.shouderW})
 
             this.cancelInfo()
         },
 
         cancelInfo:function(){
             this.editText=null
-            this.weight=''
+
             this.height=''
             this.shouderW=''
         },
